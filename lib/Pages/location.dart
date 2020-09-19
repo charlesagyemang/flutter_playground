adb1a6df6c3ccee52da96bf7dffd4a1bed430555
Work <micnkru@gmail.com>
2020-09-19 11:25:07 +0000

    Feat: get time data and perform actions raw insode loading before grouping into classes

--- lib/Pages/location.dart
+++ lib/Pages/location.dart
@@ -10,10 +10,33 @@ class Loading extends StatefulWidget {
 
 class _LoadingState extends State<Loading> {
 
+  void getTime() async {
+    Response response = await get('http://worldtimeapi.org/api/timezone/Europe/London.json');
+    Map data = jsonDecode(response.body);
+
+    //get properties
+    String dateTime = data['datetime'];
+    String offSet   = data['utc_offset'].substring(1,3);
+
+    // Create dateTime Object
+    DateTime now = DateTime.parse(dateTime);
+    now = now.add(Duration(hours: int.parse(offSet)));
+
+    print('$now');
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getTime();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Text('Loading Screen')
     );
   }
+
+
 }
